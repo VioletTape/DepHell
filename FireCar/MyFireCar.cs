1804f3b276e78f250ca0e40abc9c3a90e7d4f500
VioletTape <my@violet-tape.net>
2015-07-02 09:49:36 +0300

    Init Commit

---
+++ FireCar/MyFireCar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladderss;
+using NUnit.Framework;
+
+namespace FireCar
+{
+    [TestFixture]
+    public class MyFireCar
+    {
+        [Test]
+        public void testName() {
+            var s = new Ladder().Get();
+
+            Assert.That(s, Is.EqualTo("Fire chassis v1.0.1 and Ladder length 10"));
+
+            var s1 = new WaterTank.WaterTank().Get();
+            Assert.That(s1, Is.EqualTo("Fire chassis v1.0.1 and WaterTank length 20"));
+
+        }
+    }
+}
---
+++ FireChase/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireChase
+{
+    public class Chassis
+    {
+        public string GetChassis(string equip, int length = 20) {
+            return "Fire chassis v1.0.1 and " + equip + " length " + length.ToString();
+        }
+    }
+}
